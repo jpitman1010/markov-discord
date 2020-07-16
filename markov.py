76756eb629e2160e5f7cb3b498cf89a7543b6a21
Julie Pitman <jpitman1010@gmail.com>
2020-07-15 20:15:07 -0700

    "got the bot to work!!!"

--- markov.py
+++ markov.py
@@ -1,66 +1,89 @@
 """A Markov chain generator that can tweet random messages."""
-
+import os
+import discord
 import sys
 from random import choice
 
 
-def open_and_read_file(filenames):
-    """Take list of files. Open them, read them, and return one long string."""
 
-    body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+# def open_and_read_file(filenames):
+#     """Take list of files. Open them, read them, and return one long string."""
 
-    return body
+#     body = ''
+#     for filename in filenames:
+#         text_file = open(filename)
+#         body = body + text_file.read()
+#         text_file.close()
 
-
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains."""
-
-    chains = {}
-
-    words = text_string.split()
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
-
-        if key not in chains:
-            chains[key] = []
-
-        chains[key].append(value)
-
-    return chains
+#     return body
 
 
-def make_text(chains):
-    """Take dictionary of Markov chains; return random text."""
+# def make_chains(text_string):
+#     """Take input text as string; return dictionary of Markov chains."""
 
-    keys = list(chains.keys())
-    key = choice(keys)
+#     chains = {}
 
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text).
+#     words = text_string.split()
+#     for i in range(len(words) - 2):
+#         key = (words[i], words[i + 1])
+#         value = words[i + 2]
 
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
+#         if key not in chains:
+#             chains[key] = []
 
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
+#         chains[key].append(value)
 
-    return ' '.join(words)
+#     return chains
 
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
+# def make_text(chains):
+#     """Take dictionary of Markov chains; return random text."""
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+#     keys = list(chains.keys())
+#     key = choice(keys)
 
-# Get a Markov chain
-chains = make_chains(text)
+#     words = [key[0], key[1]]
+#     while key in chains:
+#         # Keep looping until we have a key that isn't in the chains
+#         # (which would mean it was the end of our original text).
+
+#         # Note that for long texts (like a full book), this might mean
+#         # it would run for a very long time.
+
+#         word = choice(chains[key])
+#         words.append(word)
+#         key = (key[1], word)
+
+#     return ' '.join(words)
+
+
+# # Get the filenames from the user through a command line prompt, ex:
+# # python markov.py green-eggs.txt shakespeare.txt
+# filenames = sys.argv[1:]
+
+# # Open the files and turn them into one long string
+# text = open_and_read_file(filenames)
+
+# # Get a Markov chain
+# chains = make_chains(text)
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # TODO: replace this with your code
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['DISCORD_TOKEN'])
+
